7f32bba06bca2beee8ecb0611c505f33086a9d3e
anityam <89035703+anityam@users.noreply.github.com>
2024-04-13 08:56:22 -0400

    recreate all

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "LongIsland Nepali Society",
+  description: "For long island Nepali Society",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
