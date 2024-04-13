4ad0891896290c325e7aee2aa897a8b19930158a
anityam <89035703+anityam@users.noreply.github.com>
2024-04-13 12:13:11 -0400

    doing thing

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Body = () => {
     return(
         <div class="h-svh  bg-white">
-            <div class="px-20 py-10 font-medium text-gray-500">
+            <div class="p-10 font-medium text-gray-500">
                 Under Construction
             </div>
         </div>
