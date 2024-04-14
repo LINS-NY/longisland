f2066332f28b8bfa6980060115dfc5d4010da774
anityam <89035703+anityam@users.noreply.github.com>
2024-04-14 09:10:28 -0400

    done images

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 
 const Body = () => {
     return(
-        <div class="h-svh  bg-white">
-            <div class="p-10 font-medium text-gray-500">
-                Under Construction
+        <div class="h-svh  bg-white flex">
+            <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
+                <span class="text-sm md:text-lg font-semibold whitespace-nowrap text-gray-500 dark:text-gray-400">Under Construction</span>
             </div>
         </div>
     )
