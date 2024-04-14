6c0662d3a5d32a7ae56a8c4de1667e7431877f6c
anityam <89035703+anityam@users.noreply.github.com>
2024-04-14 10:17:25 -0400

    done change

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
-import Images from 'next/image'
+import Image from 'next/image'
 
 const Calendar = () => {
     return(
-        <div class="h-svh  bg-white flex">
+        <div class="h-svh  bg-white flex flex-col">
             <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
                 <span class="text-sm md:text-lg font-semibold whitespace-nowrap text-gray-500 dark:text-gray-400">Calendar for our activity</span>
+            </div>
+            <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
+                <span class="text-sm md:text-3xl font-semibold whitespace-nowrap text-gray-500 dark:text-gray-400">Upcoming Event</span>
+            </div>
+            <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
+                <Image class="mx-1 px-5"
+            src="/new_year.jpg"
+            width={1000}
+            height={1000}
+            alt="Lins logo"
+            />
             </div>
         </div>
     )
