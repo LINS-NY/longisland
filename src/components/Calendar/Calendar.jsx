b1d8c29342d606da4ac058b92083a25a751671ed
anityam <89035703+anityam@users.noreply.github.com>
2024-04-14 10:17:56 -0400

    done things

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -5,7 +5,7 @@ const Calendar = () => {
     return(
         <div class="h-svh  bg-white flex flex-col">
             <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
-                <span class="text-sm md:text-lg font-semibold whitespace-nowrap text-gray-500 dark:text-gray-400">Calendar for our activity</span>
+                <span class="text-sm md:text-3xl font-semibold whitespace-nowrap text-gray-500 dark:text-gray-400">Calendar for our activity</span>
             </div>
             <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
                 <span class="text-sm md:text-3xl font-semibold whitespace-nowrap text-gray-500 dark:text-gray-400">Upcoming Event</span>
