955f4ce3076ddcbadcd56529caf1799e0d1fcfdd
anityam <89035703+anityam@users.noreply.github.com>
2024-04-14 10:04:18 -0400

    add the pages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Link from 'next/link'
 
 
 const Footer = () => {
@@ -7,14 +8,14 @@ const Footer = () => {
 
         <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
             <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 LINS. All Rights Reserved.
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 LINS-NY. All Rights Reserved.
             </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                    <Link href="/About" class="hover:underline me-4 md:me-6">About</Link>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline">Contact</a>
+                    <Link href="/About" class="hover:underline">Contact</Link>
                 </li>
             </ul>
             </div>
