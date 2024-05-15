54ca084c2b4c1247916d5c7f7f330e3af1318c8c
anityam <89035703+anityam@users.noreply.github.com>
2024-05-14 21:09:12 -0400

    fix case

--- src/components/Member/LifeTimeMember.jsx
+++ src/components/Member/LifeTimeMember.jsx
@@ -4,7 +4,7 @@ const LifeTimeMember = ({id,name}) => {
     return(
         <tr class="border-b border-neutral-200 bg-black/[0.02] dark:border-white/10">
             <td class="whitespace-nowrap px-6 py-4 font-medium">{id}</td>
-            <td class="whitespace-nowrap px-6 py-4 font-bold text-teal-600">{name}</td>
+            <td class="capitalize whitespace-nowrap px-6 py-4 font-bold text-teal-600">{name}</td>
             <td class="whitespace-nowrap px-6 py-4 font-bold text-teal-600">NEVER</td>
             <td class="whitespace-nowrap px-6 py-4 font-bold text-teal-600">LIFE TIME</td>
         </tr>
