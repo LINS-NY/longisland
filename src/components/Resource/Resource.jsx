d441c1517c236cc1b7f3b8a79a6caef0acd31176
anityam <89035703+anityam@users.noreply.github.com>
2024-05-15 20:02:15 -0400

    fixing link back

--- src/components/Resource/Resource.jsx
+++ src/components/Resource/Resource.jsx
@@ -27,32 +27,32 @@ const Resource = () => {
                             {/* Home Page */}
                             <p class = "whitespace-pre">  </p>
                             <p class = "flex text-bold whitespace-pre">LINK: </p>
-                            <a href="https://www.uscis.gov" class="font-medium text-white-600 dark:text-blue-500 hover:underline">https://www.uscis.gov/</a>
+                            <a href="https://www.uscis.gov" rel="noopener noreferrer" class="font-medium text-white-600 dark:text-blue-500 hover:underline">https://www.uscis.gov/</a>
                             
                             {/* ALL Florm */}
                             <p class = "whitespace-pre">  </p>
                             <p class = "flex text-bold whitespace-pre">All Forms: </p>
-                            <a href="https://www.uscis.gov/forms/all-forms" class="font-medium text-white-600 dark:text-blue-500 hover:underline">https://www.uscis.gov/forms/all-forms</a>
+                            <a href="https://www.uscis.gov/forms/all-forms" rel="noopener noreferrer" class="font-medium text-white-600 dark:text-blue-500 hover:underline">https://www.uscis.gov/forms/all-forms</a>
 
                             {/* Case Status Online */}
                             <p class = "whitespace-pre">  </p>
                             <p class = "flex text-bold whitespace-pre">Case Status Online: </p>
-                            <a href="https://egov.uscis.gov/" class="font-medium text-white-600 dark:text-blue-500 hover:underline">https://egov.uscis.gov/</a>
+                            <a href="https://egov.uscis.gov/" rel="noopener noreferrer" class="font-medium text-white-600 dark:text-blue-500 hover:underline">https://egov.uscis.gov/</a>
 
                             {/* Check Case Processing Times */}
                             <p class = "whitespace-pre">  </p>
                             <p class = "flex text-bold whitespace-pre">Check Case Processing Times: </p>
-                            <a href="https://egov.uscis.gov/processing-times/" class="font-medium text-white-600 dark:text-blue-500 hover:underline">https://egov.uscis.gov/processing-times/</a>
+                            <a href="https://egov.uscis.gov/processing-times/" rel="noopener noreferrer" class="font-medium text-white-600 dark:text-blue-500 hover:underline">https://egov.uscis.gov/processing-times/</a>
                             
                             {/* How to Change Your Address */}
                             <p class = "whitespace-pre">  </p>
                             <p class = "flex text-bold whitespace-pre">How to Change Your Address: </p>
                             <p class = "flex whitespace-pre px-5">Paper Format: </p>
-                            <a href="https://www.uscis.gov/ar-11" class="font-medium px-5 text-white-600 dark:text-blue-500 hover:underline">https://www.uscis.gov/ar-11</a>
+                            <a href="https://www.uscis.gov/ar-11" rel="noopener noreferrer" class="font-medium px-5 text-white-600 dark:text-blue-500 hover:underline">https://www.uscis.gov/ar-11</a>
 
                             <p class = "whitespace-pre">  </p>
                             <p class = "flex whitespace-pre px-5">Online through a USCIS online account: </p>
-                            <a href="https://www.uscis.gov/addresschange" class="font-medium px-5 text-white-600 dark:text-blue-500 hover:underline">https://www.uscis.gov/addresschange</a>
+                            <a href="https://www.uscis.gov/addresschange" rel="noopener noreferrer" class="font-medium px-5 text-white-600 dark:text-blue-500 hover:underline">https://www.uscis.gov/addresschange</a>
 
                         </th>
                     </tr>
