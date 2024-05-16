cd7287dce56aead61a32747996eb75256901ac04
anityam <89035703+anityam@users.noreply.github.com>
2024-05-15 23:03:18 -0400

    added fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,31 +7,41 @@ const Header = () => {
         <header>
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap xl:flex-row flex-col items-center md:justify-between mx-auto p-4">
-            <div class= "flex sm:flex-row justify-between p-4">
+            <div class= "hidden sm:flex flex-row justify-between p-4">
                 <Image class="mx-1"
                 src="/LINS.png"
                 width={100}
                 height={100}
                 alt="Lins logo"
                 />
-                <span class="self-center text-sm md:text-lg font-extrabold whitespace-nowrap dark:text-white text-blue-900">Long Island Nepalese Society, New York<p>लङ्ग आईल्यान्ड नेपाली समाज, न्युयोर्क</p></span>
+                <span class="self-center text-lg font-extrabold whitespace-nowrap dark:text-white text-blue-900">Long Island Nepalese Society, New York<p>लङ्ग आईल्यान्ड नेपाली समाज, न्युयोर्क</p></span>
             </div>
+            <div class= "sm:hidden flex p-1">
+                <Image class="mx-1"
+                src="/LINS.png"
+                width={70}
+                height={70}
+                alt="Lins logo"
+                />
+                <span class="self-center text-xs  font-bold whitespace-nowrap dark:text-white text-blue-900">Long Island Nepalese Society, New York<p>लङ्ग आईल्यान्ड नेपाली समाज, न्युयोर्क</p></span>
+            </div>
+            
             <div class="md:hidden">
-                <ul class="font-medium text-sm flex flex-row p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 rtl:space-x-reverse    dark:bg-gray-800  dark:border-gray-700">
+                <ul class="text-sm flex flex-row  mt-1 border border-gray-100 rounded-lg bg-gray-50 rtl:space-x-reverse    dark:bg-gray-800  dark:border-gray-700">
                     <li>
-                    <Link href="/" class="block py-2 px-3  text-gray-900 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
+                    <Link href="/" class="block p-2  text-gray-900 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
                     </li>
                     <li>
-                    <Link href="/Resources" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white ">Resources</Link>
+                    <Link href="/Resources" class="block p-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white ">Resources</Link>
                     </li>
                     <li>
-                    <Link href="/Members" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white ">Members</Link>
+                    <Link href="/Members" class="block p-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white ">Members</Link>
                     </li>
                     <li>
-                    <Link href="/Donations" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white ">Donation</Link>
+                    <Link href="/Donations" class="block p-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white ">Donation</Link>
                     </li>
                     <li>
-                    <Link href="/Calendar" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white ">Calendar</Link>
+                    <Link href="/Calendar" class="block p-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white ">Calendar</Link>
                     </li>
                 </ul>
             </div>
