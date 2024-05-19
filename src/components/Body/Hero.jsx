7f877091b0d5eb0c2e2fd10a97ebc06427f61ccc
anityam <89035703+anityam@users.noreply.github.com>
2024-05-18 23:03:49 -0400

    small screen image

--- src/components/Body/Hero.jsx
+++ src/components/Body/Hero.jsx
@@ -3,33 +3,53 @@ import Image from 'next/image'
 
 const Hero =()=>{
    return(
-    <section class="container rounded-xl mx-auto  w-5/6 h-64 shadow-sm bg-gradient-to-r from-purple-500 to-red-500">
-       <div class="flex justify-between m-2 h-full">
-        <div class="flex justify-center">
-            <div class="flex flex-col sm:mx-10 justify-center">
-                <h1 class="max-w-2xl mb-2 text-lg md:text-2xl font-extrabold tracking-tight leading-none  text-blue-900 xl:text-3xl dark:text-white">
-                Long Island Nepalese Society, New York
-                </h1>
-                <p class="max-w-2xl mb-4 text-lg md:text-2xl font-extrabold tracking-tight leading-none text-blue-900 xl:text-3xl dark:text-white">
-                लङ्ग आईल्यान्ड नेपाली समाज, न्युयोर्क
-                </p>
-                <p class="max-w-2xl mb-4 text-sm fond-sm tracking-tight leading-none text-blue-900 xl:text-2xl dark:text-white">
-                To Serve, Unite and move toward progress
-                </p>
+    <div>
+        <section class="sm:hidden relative container rounded-xl mx-auto  my-5  w-5/6  h-96 shadow-sm dark:bg-gray-900 bg-cover bg-no-repeat bg-center" style={{backgroundImage: `url('./Designer.jpeg')`}}>
+        <div class="relative flex justify-between m-2  h-full mx-auto backdrop-blur-md bg-white/60 dark:bg-slate-700/60">
+            <div class="flex justify-center font-bold">
+                <div class="flex flex-col text-center mx-5 justify-center item-center">
+                    <h1 class="sm:max-w-2xl mb-2 text-md tracking-tight leading-none text-blue-900  dark:text-white">
+                    Long Island Nepalese Society
+                    </h1>
+                    <p class="mb-4 text-md tracking-tight leading-none text-blue-900  dark:text-white"> New York</p>
+                    <p class=" mb-4 text-md tracking-tight leading-none text-blue-900  dark:text-white">
+                    लङ्ग आईल्यान्ड नेपाली समाज, न्युयोर्क
+                    </p>
+                    <p class="max-w-2xl mb-4 text-sm fond-sm tracking-tight leading-none text-blue-900 xl:text-2xl dark:text-white">
+                    To Serve, Unite and move toward progress
+                    </p>
+                </div>
+            </div>
+            </div>
+        </section>
+        <section class="hidden sm:flex relative container rounded-xl mx-auto  my-5 sm:my-10 sm:w-3/5 w-5/6 sm:h-64 h-96 shadow-sm dark:bg-gray-900 bg-gradient-to-r from-purple-500 to-red-500 ">
+        <div class="relative flex justify-between m-2 sm:w-5/6 h-full mx-auto">
+            <div class="flex justify-center ">
+                <div class="flex flex-col sm:mx-10 mx-5 justify-center item-center">
+                    <h1 class=" mb-2 text-md md:text-2xl sm:font-extrabold tracking-tight leading-none text-blue-900 xl:text-3xl dark:text-white">
+                    Long Island Nepalese Society, New York
+                    </h1>
+                    <p class="sm:max-w-2xl mb-4 text-md md:text-2xl sm:font-extrabold tracking-tight leading-none text-blue-900 xl:text-3xl dark:text-white">
+                    लङ्ग आईल्यान्ड नेपाली समाज, न्युयोर्क
+                    </p>
+                    <p class="max-w-2xl mb-4 text-sm fond-sm tracking-tight leading-none text-blue-900 xl:text-2xl dark:text-white">
+                    To Serve, Unite and move toward progress
+                    </p>
+                </div>
+            </div>
+            <div class=" hidden sm:flex sm:mx-5 justify-center">
+                <div class="flex flex-col justify-center">
+                    <Image 
+                        src="/LINS.png"
+                        width={200}
+                        height={200}
+                        alt="Lins logo"
+                        />
+                </div>
             </div>
         </div>
-        <div class=" hidden sm:flex sm:mx-5 justify-center">
-            <div class="flex flex-col justify-center">
-                <Image 
-                    src="/LINS.png"
-                    width={200}
-                    height={200}
-                    alt="Lins logo"
-                    />
-            </div>
-        </div>
-       </div>
-    </section>
+        </section>
+    </div>
     )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
                 height={100}
                 alt="Lins logo"
                 />
-                <span class="self-center text-lg font-extrabold whitespace-nowrap dark:text-white text-blue-900">Long Island Nepalese Society, New York<p>लङ्ग आईल्यान्ड नेपाली समाज, न्युयोर्क</p></span>
+                <span class="self-center text-lg font-extrabold whitespace-nowrap  text-blue-900 dark:text-white">Long Island Nepalese Society, New York<p>लङ्ग आईल्यान्ड नेपाली समाज, न्युयोर्क</p></span>
             </div>
             <div class= "sm:hidden flex p-1">
                 <Image class="mx-1"
@@ -29,7 +29,7 @@ const Header = () => {
             <div class="md:hidden">
                 <ul class="text-sm sm:font-bold sm:text-lg flex flex-row  mt-1 border border-gray-100 rounded-lg bg-gray-50 rtl:space-x-reverse    dark:bg-gray-800  dark:border-gray-700">
                     <li>
-                    <Link href="/" class="block p-2 sm:p-4  text-gray-900 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
+                    <Link href="/" class="block p-2 sm:p-4  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white  " aria-current="page">Home</Link>
                     </li>
                     <li>
                     <Link href="/Resources" class="block p-2 sm:p-4  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white ">Resources</Link>
@@ -48,7 +48,7 @@ const Header = () => {
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                <Link href="/" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
+                <Link href="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Home</Link>
                 </li>
                 <li>
                 <Link href="/Resources" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Resources</Link>
