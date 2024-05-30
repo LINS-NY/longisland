29f9ee4df5df39062768778c349522df10057dc4
anityam <89035703+anityam@users.noreply.github.com>
2024-05-30 18:08:06 -0400

    add done

--- src/components/Body/Hero.jsx
+++ src/components/Body/Hero.jsx
@@ -7,7 +7,7 @@ const Hero =()=>{
     <div>
         <section class="sm:hidden relative container rounded-xl mx-auto  my-5  w-5/6  h-96 shadow-sm dark:bg-gray-900 bg-cover bg-no-repeat bg-center" style={{backgroundImage: `url('./Designer.jpeg')`}}>
         <div class="relative flex justify-between m-2  h-full mx-auto backdrop-blur-md bg-white/60 dark:bg-slate-700/60">
-            <div class="flex justify-center font-bold">
+            <div class="flex flex-grow justify-center font-bold">
                 <div class="flex flex-col text-center mx-5 justify-center item-center">
                     <h1 class="sm:max-w-2xl mb-2 text-md tracking-tight leading-none text-blue-900  dark:text-white">
                     Long Island Nepalese Society
@@ -19,24 +19,26 @@ const Hero =()=>{
                     <p class="max-w-2xl mb-4 text-sm fond-sm tracking-tight leading-none text-blue-900 xl:text-2xl dark:text-white">
                     From Planning To Progress
                     </p>
-                    <br/>
-                    <div class="-space-x-1">
-                        <Link href="/FinancialReport">class='btn'
-                            <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 
-                                    border-blue-700 hover:border-blue-500 rounded">Financial Report</button>
+                    <div class="flex flex-col">
+                        <Link href="/FinancialReport">
+                            <button class="bg-blue-500 hover:bg-blue-400 text-white font-small md:font-bold md:p-4 border-b-4 my-2
+                                    border-blue-700 hover:border-blue-500 rounded">
+                                        Financial Report
+                                        </button>
                         </Link>
-                        <Link href="/About">class='btn'
-                            <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-3 border-b-4 
+                        <Link href="/About">
+                            <button class="bg-blue-500 hover:bg-blue-400 text-white font-medium md:font-bold md:py-2 md:px-4 border-b-4 my-2
                                     border-blue-700 hover:border-blue-500 rounded">Membership Form</button>
                         </Link>
-                        <Link href="/About">class='btn'
-                            <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-3 border-b-4 
+                        <Link href="/About">
+                            <button class="bg-blue-500 hover:bg-blue-400 text-white font-medium md:font-bold md:py-2 md:px-4 border-b-4 my-2
                                     border-blue-700 hover:border-blue-500 rounded">Donation Form</button>
                         </Link>
+                    </div>
                 </div>
-                </div>
+                
             </div>
-            </div>
+        </div>
         </section>
         <section class="hidden sm:flex relative container rounded-xl mx-auto  my-5 sm:my-10 sm:w-3/5 w-5/6 sm:h-64 h-96 shadow-sm dark:bg-gray-900 bg-gradient-to-r from-purple-500 to-red-500 ">
         <div class="relative flex justify-between m-2 sm:w-5/6 h-full mx-auto">
