2e00e46f2c4d9706929a63a2b660f64b4e67c698
LINS-NY <longislandnepalese@gmail.com>
2024-05-31 13:31:22 -0400

    removed text this is the main page

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -5,10 +5,6 @@ const Body = () => {
     return(
         <div class=" bg-white flex flex-col">
             <Hero/>
-            <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
-                <span class="text-sm md:text-lg font-semibold whitespace-nowrap text-gray-500 dark:text-gray-400">This is the main Page.</span>
-                
-            </div>
         </div>
     )
 }
