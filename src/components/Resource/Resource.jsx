5879d29f770fcb57bcb703a4a8f93f3ab3fabb3a
anityam <89035703+anityam@users.noreply.github.com>
2024-05-31 14:43:18 -0400

    remove shadow

--- src/components/Resource/Resource.jsx
+++ src/components/Resource/Resource.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Resource = ({item}) => {
     return(
       <div class="flex w-96 sm:w-5/6 mx-auto">
-        <div class="flex flex-col justify-center item-center rounded-xl drop-shadow-xl dark:bg-gray-900 mt-2 sm:mt-5 shadow-2xl shadow-slate-50">
+        <div class="flex flex-col justify-center item-center rounded-xl  dark:bg-gray-900 mt-2 sm:mt-5 shadow-2xl shadow-slate-50">
             <div class="mt-1 md:mt-5 md:p-4 p-1 justify-center mx-auto ">
                 <h1 class="text-lg md:text-5xl font-semibold whitespace-nowrap text-black dark:text-white">{item.Title}</h1>
             </div>
