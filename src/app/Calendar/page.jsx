18645011b5bd67879892177524158bef443f3631
anityam <89035703+anityam@users.noreply.github.com>
2024-06-01 07:52:25 -0400

    calendar demo

--- src/app/Calendar/page.jsx
+++ src/app/Calendar/page.jsx
@@ -4,7 +4,7 @@ import Footer from "../../components/Footer/Footer"
 
 export default function Calendars() {
   return (
-    <main class="flex flex-col h-screen overflow-hidden">
+    <main class="flex flex-col overflow-hidden">
       <Header/>
       <Calendar/>
       <Footer/>
--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,22 +1,47 @@
 import * as React from 'react';
 import Image from 'next/image'
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const currentDay= new Date()
+
+const Today = () => {
+    return (
+        <div>
+            <h1 class="text-center font-bold text-4xl">Today</h1>
+            <div class="flex flex-col container border-2 drop-shadow-2xl border-stone-300 shadow-slate-900 bg-orange-200 w-48 sm:w-96 h-52 md:h-96 mt-2">
+                    <div class="text-xl md:text-9xl   shadow-slate-900 bg-orange-300 border-stone-300 ">
+                    {months[currentDay.getMonth()]}
+                    </div>
+                    <div>
+                        {weekdays[currentDay.getDay()]}
+                    </div>
+                    <div>
+                        {currentDay.getDate()}
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+const CalendarDisplay = () => {
+   
+   
+   console.log(currentDay.getDay())
+    return (
+        <div>
+            <h1>calendar</h1>
+        </div>
+    )
+}
 
 const Calendar = () => {
     return(
-        <div class="h-svh  bg-white flex flex-col">
-            <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
-                <span class="text-sm md:text-3xl font-semibold whitespace-nowrap text-gray-500 dark:text-gray-400">Calendar for our activity</span>
+        <div class="bg-white flex flex-col">
+            <div class="flex flex-row justify-between mx-auto p-4">
+                <Today/>
             </div>
-            <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
+            <div class="flex flex-row justify-between mx-auto p-4">
                 <span class="text-sm md:text-3xl font-semibold whitespace-nowrap text-gray-500 dark:text-gray-400">Upcoming Event</span>
-            </div>
-            <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
-                <Image class="mx-1 px-5"
-            src="/new_year.jpg"
-            width={1000}
-            height={1000}
-            alt="Lins logo"
-            />
             </div>
         </div>
     )
