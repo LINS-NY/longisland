59ea525687b20807bc745f694bc335843eb34c0c
anityam <89035703+anityam@users.noreply.github.com>
2024-06-10 11:06:26 -0400

    done fixing

--- src/components/FinancialReport/FinancialReport.jsx
+++ src/components/FinancialReport/FinancialReport.jsx
@@ -1,39 +1,39 @@
 import * as React from 'react';
 
 const FinancialReport = ({title,content,heading}) => {
-    console.log(title,content,heading)
     const contTable = []
     const headItem = 
         <tr class="rounded-3xl">   
-            <th scope="col" class=" font-bold md:px-6 px-1 py-1 md:py-4">{heading[0]}</th>
-            <th scope="col" class="font-bold md:px-6 px-1 py-1 md:py-4">{heading[1]}</th>
-            <th scope="col" class="font-bold md:px-6 px-1 py-1 md:py-4">{heading[2]}</th>
-            <th scope="col" class="font-bold md:px-6 px-1 py-1 md:py-4">{heading[3]}</th>
-            <th scope="col" class="font-bold md:px-6 px-1 py-1 md:py-4">{heading[4]}</th>
+            <th scope="col" class="font-small md:px-6  md:py-4 ">
+                <span class="text-sm">{heading[0]}</span></th>
+            <th scope="col" class="font-small sm:font-bold md:px-6  md:py-4 text-sm">{heading[1]}</th>
+            <th scope="col" class="font-small sm:font-bold md:px-6  md:py-4 text-sm">{heading[2]}</th>
+            <th scope="col" class="font-small sm:font-bold md:px-6  md:py-4 text-sm">{heading[3]}</th>
+            <th scope="col" class="font-small sm:font-bold md:px-6  md:py-4 text-sm">{heading[4]}</th>
         </tr>
 
-   content.map((val) => {
+   content.map((val,index) => {
+
         contTable.push(
-        <tr class="border-b border-neutral-00 bg-black/[0.02] dark:border-white/10 hover:bg-cyan-200">
-            <td class="capitalize text-black lg:whitespace-nowrap lg:px-6 px-1 lg:py-4 py-1 font-medium">{val[0]}</td>
-            <td class="lg:whitespace-nowrap text-black lg:px-6 px-1 lg:py-4 py-1 font-medium">{val[1]}</td>
-            <td class="lg:whitespace-nowrap text-black lg:px-6 px-1 lg:py-4 py-1 font-medium">{val[2]}</td>
-            <td class="lg:whitespace-nowrap text-black lg:px-6 px-1 lg:py-4 py-1 font-medium">{val[3]}</td>
-            <td class="lg:whitespace-nowrap text-black lg:px-6 px-1 lg:py-4 py-1 font-medium">{val[4]}</td>
-        </tr>
+            <tr class="border-b border-neutral-00 bg-black/[0.02] dark:border-white/10 hover:bg-cyan-200 " key={index}>
+                <td class=" text-black lg:whitespace-nowrap lg:px-6 px-1 lg:py-4 sm:font-medium" key={index}>{val[0]}</td>
+                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 sm:font-medium" key={index}>{val[1]}</td>
+                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 sm:font-medium" key={index}>{val[2]}</td>
+                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 sm:font-medium" key={index}>{val[3]}</td>
+                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 sm:font-medium" key={index}>{val[4]}</td>
+            </tr>
         )
     })  
-    console.log(contTable)
     return(
-        <div class=" bg-white flex flex-row w-96 sm:w-5/6">
+        <div class=" bg-white flex flex-row justify-center text-center sm:px-6">
             <div class="p-4">
                 <h1 class="text-lg sm:text-xl font-semibold whitespace-nowrap text-gray-500 mx-auto text-center dark:text-gray-400">
                     {title}
                 </h1>
-                <div class="mt-5 sm:mt-20 shadow-lg shadow-cyan-500/50 ">
+                <div class="mt-5 sm:mt-20 shadow-lg shadow-cyan-500/50 rounded-3xl">
                     <div class="overflow-hidden ">
-                        <table class="w-full text-center text-sm font-light dark:text-white border-collapse ">
-                            <thead class="shadow-lg shadow-cyan-500/50 hover:bg-cyan-600 bg-sky-500/100 rounded-3xl">                                    
+                        <table class="w-full text-center text-sm font-light dark:text-white border-collapse rounded-3xl">
+                            <thead class=" hover:bg-cyan-600 bg-sky-500/100 rounded-3xl">                                    
                                 {headItem}
                             </thead>
                             <tbody> 
