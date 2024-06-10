db404b91591721343098bd3b62ae88283d932d22
anityam <89035703+anityam@users.noreply.github.com>
2024-06-10 11:16:45 -0400

    make center

--- src/components/FinancialReport/FinancialReport.jsx
+++ src/components/FinancialReport/FinancialReport.jsx
@@ -13,14 +13,13 @@ const FinancialReport = ({title,content,heading}) => {
         </tr>
 
    content.map((val,index) => {
-
         contTable.push(
             <tr class="border-b border-neutral-00 bg-black/[0.02] dark:border-white/10 hover:bg-cyan-200 " key={index}>
-                <td class=" text-black lg:whitespace-nowrap lg:px-6 px-1 lg:py-4 sm:font-medium" key={index}>{val[0]}</td>
-                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 sm:font-medium" key={index}>{val[1]}</td>
-                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 sm:font-medium" key={index}>{val[2]}</td>
-                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 sm:font-medium" key={index}>{val[3]}</td>
-                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 sm:font-medium" key={index}>{val[4]}</td>
+                <td class=" text-black lg:whitespace-nowrap lg:px-6 px-1 lg:py-4 text-sm sm:font-medium" key={index}>{val[0]}</td>
+                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 text-sm sm:font-medium" key={index}>{val[1]}</td>
+                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 text-sm sm:font-medium" key={index}>{val[2]}</td>
+                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 text-sm sm:font-medium" key={index}>{val[3]}</td>
+                <td class="lg:whitespace-nowrap text-black lg:px-6  lg:py-4 text-sm sm:font-medium" key={index}>{val[4]}</td>
             </tr>
         )
     })  
--- src/components/FinancialReports/FinancialReports.jsx
+++ src/components/FinancialReports/FinancialReports.jsx
@@ -14,7 +14,7 @@ export default async function FinancialReports(){
     return(
         <div class="container my-5 sm:my-12 mx-auto px-4 md:px-12">
             <h1 class="text-center text-lg py-4 lg:text-5xl lg:py-10 font-bold whitespace-nowrap text-gray-500 dark:text-gray-400">Reports</h1>
-            <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <div class="flex flex-wrap -mx-1 lg:-mx-4 justify-center">
                 <div class="grid grid-cols-1 sm:grid-cols-4 gap-4 px-1 ">
                     {item}
                 </div>
