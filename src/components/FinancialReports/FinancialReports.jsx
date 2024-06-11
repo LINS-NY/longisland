1e407c96b08a77ee713f3ace2de7d0efe083b903
anityam <89035703+anityam@users.noreply.github.com>
2024-06-11 14:19:42 -0400

    added seperate bank details

--- src/components/FinancialReports/FinancialReports.jsx
+++ src/components/FinancialReports/FinancialReports.jsx
@@ -8,15 +8,27 @@ export default async function FinancialReports(){
         return (await getFinancialDocs(document.params.documentId))
     }))
     let item = []
+    let financialItem = []
     docsData.map((i)=> {
-       item.push(<Item title={i.title} location={i.location} key={i.location}/>)
+        if (i.title.includes("Checking")){
+            financialItem.push(<Item title={i.title} location={i.location} key={i.location}/>)
+        }else{
+            item.push(<Item title={i.title} location={i.location} key={i.location}/>)
+        }
     })
     return(
         <div class="container my-5 sm:my-12 mx-auto px-4 md:px-12">
             <h1 class="text-center text-lg py-4 lg:text-5xl lg:py-10 font-bold whitespace-nowrap text-gray-500 dark:text-gray-400">Reports</h1>
+            <h2 class="text-center text-lg py-4 lg:text-5xl lg:py-10 font-bold whitespace-nowrap text-gray-500 dark:text-gray-400">Events Financial Details</h2>
             <div class="flex flex-wrap -mx-1 lg:-mx-4 justify-center">
                 <div class="grid grid-cols-1 sm:grid-cols-4 gap-4 px-1 ">
                     {item}
+                </div>
+            </div>
+            <h2 class="text-center text-lg py-4 lg:text-5xl lg:py-10 font-bold whitespace-nowrap text-gray-500 dark:text-gray-400">Bank Financial Details</h2>
+            <div class="flex flex-wrap -mx-1 lg:-mx-4 justify-center">
+                <div class="grid grid-cols-1 sm:grid-cols-4 gap-4 px-1 ">
+                    {financialItem}
                 </div>
             </div>
         </div>
