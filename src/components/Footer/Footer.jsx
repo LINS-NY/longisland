cbd9072440c9fe2455686f254aeb5a0ca6e6d092
anityam <89035703+anityam@users.noreply.github.com>
2025-11-18 10:00:47 -0600

    update year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import Link from 'next/link'
 
 
 const Footer = () => {
+    const year = new Date().getFullYear();
     return(
-        
 
         <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-white ">© 2024 LINS-NY. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-white ">© {year} LINS-NY. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-white sm:mt-0">
                 <li>
