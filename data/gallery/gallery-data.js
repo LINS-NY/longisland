d9bcf24912bdd87ec610a27229f2793d01e457bb
LINS-NY <longislandnepalese@gmail.com>
2026-04-17 14:23:18 -0400

    pictures added (#126)
    Co-authored-by: SareePasal <sareepasalusa@gmail.com>

--- data/gallery/gallery-data.js
+++ data/gallery/gallery-data.js
@@ -1,4 +1,11 @@
 const galleryData = {
+  "2023": [
+    {
+      event: "Nepali Bhasa Graduation Ceremony 2023",
+      driveFolderId: "15N9QsXcVXunwN1Dk2XNIMDwNa4SUv9ON",
+      description: "Nepali language learners celebrated their graduation with a ceremony recognizing their dedication and achievements in mastering the language."
+    },
+  ],
   "2024": [
     {
       event: "Nepali New Year 2081",
@@ -6,22 +13,22 @@ const galleryData = {
       description: "Welcoming the Nepali New Year 2081 with community cultural performances and traditional food."
     },
     {
-      event: "Blood Drive",
+      event: "Blood Drive 2024",
       driveFolderId: "11xhpPauuxTahiaw9rSJuKkPa3zCzsbHq",
       description: "A successful community health initiative focused on life-saving blood donations from our members."
     },
     {
-      event: "Immigration Rights Interaction Program",
+      event: "Immigration Rights Interaction Program 2024",
       driveFolderId: "1msMfEGntd0zncsBVQUlgWckExa5S7vN4",
       description: "An informative session helping community members understand legal rights and immigration pathways."
     },
     {
-      event: "Nepali Teej Celebration",
+      event: "Nepali Teej Celebration 2024",
       driveFolderId: "1KRoVLidg6Jwa3KP0H8VNJAhJVkDmJAlD",
       description: "Celebrating the traditional Teej festival with music, dance, and a vibrant community gathering for women."
     },
     {
-      event: "Summer Picnic",
+      event: "Summer Picnic 2024",
       driveFolderId: "1vltUBrXjcxhB1S-e0sOrAWTOkzhIaI8P",
       description: "A relaxing outdoor day at the park featuring traditional games, BBQ, and community networking."
     },
@@ -31,19 +38,19 @@ const galleryData = {
       description: "Celebrating our biggest festival, Dashain, with cultural performances, tika, and community blessings."
     },
     {
-      event: "Nepali Bhasa Graduation Ceremony",
+      event: "Nepali Bhasa Graduation Ceremony 2024",
       driveFolderId: "1v3d5UgTR127i4-P0JVbj9pU4O69XgDj9",
       description: "Nepali language learners celebrated their graduation with a ceremony recognizing their dedication and achievements in mastering the language."
     }
   ],
   "2025": [
     {
-      event: "Nepali Bhasa Graduation Ceremony",
+      event: "Nepali Bhasa Graduation Ceremony 2025",
       driveFolderId: "1Qit50D7NnPerY3NtmJP_zQV9Nb-aVdOI",
       description: "Nepali language learners celebrated their graduation with a ceremony recognizing their dedication and achievements in mastering the language."
     },
     {
-      event: "LINS 2nd Soccer Tournament",
+      event: "LINS 2nd Soccer Tournament 2025",
       driveFolderId: "1-BqCzjNOxRewrkVIt0IVUDXT5tWeqCdi",
       description: "Our annual sports event bringing together youth and adults for a day of competitive and fun soccer."
     },
@@ -53,15 +60,37 @@ const galleryData = {
       description: "A celebration of women's achievements and contributions. The event featured inspiring speakers, cultural performances, and a community discussion"
     },
     {
-       event: "Summer Picnic",
+       event: "Summer Picnic 2025",
        driveFolderId: "1_kB5cMCYkvXM7Ru5q0YH-EFuySdholtQ",
        description: "A relaxing outdoor day at the park featuring traditional games, BBQ, and community networking."
     },
     {
-       event: "Teej Celebration",
+       event: "Teej Celebration 2025",
        driveFolderId: "1i5dWcyJoWdlHD0xxEMK2DWtRCuMohyaD",
        description: "Celebrating the traditional Teej festival with music, dance, and a vibrant community gathering for women."
+    },
+    {
+       event: "Tihar Celebration 2025",
+       driveFolderId: "1GyezmSgCPbpeiNWwgWAVwJVW3Hy992Us",
+       description: "Celebrating the festival of lights, Tihar, with cultural performances, traditional rituals, and a community feast."
+    },
+    {
+       event: "Holi Celebration 2025",
+       driveFolderId: "18y5d5lH5fNeYsNVuLXCJLg18OqwTqLp3",
+       description: "Celebrating the festival of colors, Holi, with vibrant festivities, music, dance, and a joyful community gathering."
+    },
+    {
+       event: "Swasthani Mahotsav 2025",
+       driveFolderId: "19YV07eEcsXIDUkbLEwuMLvnHsiLajRWl",
+       description: "A cultural and religious event celebrating the Swasthani Mahotsav, featuring traditional rituals, storytelling, and community bonding."
     }
+  ],
+  "2026": [
+    {
+      event: "Nepali New Year 2083",
+      driveFolderId: "1cKbWGa8VUz0gm7KLYIXLF6vKDlRzLLOA",
+      description: "Welcoming the Nepali New Year 2083 with community cultural performances and traditional food."
+    },
   ]
 };
 export default galleryData;
